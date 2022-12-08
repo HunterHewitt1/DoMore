9840162851e784d176ecef5896a5e36a9db96e0c
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-12-08 10:10:31 -0700

    UserAccount model completed

---
+++ vite-project/backend/models/UserAccount.js
@@ -0,0 +1,11 @@
+const { Schema } = require('mongoose')
+
+const UserAccount = new Schema({
+  username: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+module.exports = UserAccount
--- vite-project/backend/server.js
+++ vite-project/backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
@@ -11,4 +12,10 @@ app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
+})
+
+//Routes//
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'This route is being hit' })
 })
