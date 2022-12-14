eff79ed115510f40f8e437dc3f0e3ea9563faa9e
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-12-14 10:54:05 -0700

    update task

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -3,27 +3,61 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../globals'
 import TaskCard from '../components/TaskCards'
+import { useParams } from 'react-router-dom'
 
-// const [task, updateTask] = useState([])
-// const [formState, setFormState] = useState({
-//   userAccount_id: '',
-//   taskName: "",
-//   taskDescription: "",
-//   taskDueDate: "",
-//   taskCompleted: false,
-// })
-
-// const handleUpdate = async (e) => {
-//   e.preventDefault()
-//   let updatedTask = await axios.put(`${BASE_URL}/tasks/${id}`, formState)
-//   updateTask(updatedTask.data)
-// }
 
 const Tasks = () => {
+let { id } = useParams()
+console.log(id)
+const [task, updateTask] = useState([])
+const [formState, setFormState] = useState({
+  taskName: "",
+  taskDescription: "",
+  taskDueDate: "",
+  taskCompleted: false,
+})
+
+const handleUpdate = async (e) => {
+  e.preventDefault()
+  let updatedTask = await axios.put(`${BASE_URL}/tasks/${id}`, formState)
+  updateTask(task, updatedTask)
+  setFormState({  
+  taskName: "",
+  taskDescription: "",
+  taskDueDate: "",
+  taskCompleted: false})
+}
+const handleChange = async (event) => {
+  setFormState({ ...formState, [event.target.id]: event.target.value })
+}
 
   return(
     <div>
-      <div>Tasks Test</div>
+      <div>Tasks Details</div>
+
+      <h4>Update Task</h4>
+      <form onSubmit={handleUpdate}>
+        <label htmlFor="taskName">Task Name</label>
+        <input id="taskName" type="text" value={formState.taskName} onChange={handleChange} />
+        
+        <label htmlFor="taskDescription">Task Description</label>
+        <input id="taskDescription" type="text" value={formState.taskDescription} onChange={handleChange} />
+
+        <label htmlFor="taskDueDate">Task Due Date</label>
+        <input id="taskDueDate" type="date" value={formState.taskDueDate} onChange={handleChange} />
+
+        <label htmlFor="taskCompleted">Task Completed</label>
+        <input id="taskCompleted" type="checkbox" value={formState.taskCompleted} onChange={handleChange} />
+
+        <button type="submit">Update Task</button>
+
+
+
+
+
+
+
+        </form>
     </div>
   )
 }
